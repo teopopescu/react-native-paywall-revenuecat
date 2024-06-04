e3d6ceca9747dada96c0621cfca340a92d55dced
teopopescu <teodor.popescu94@yahoo.com>
2024-06-04 12:04:18 +0100

    Add changes to index

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,7 +4,24 @@ import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
 import RevenueCatUI, {PAYWALL_RESULT} from 'react-native-purchases-ui';
 import { Alert } from 'react-native';
+import Purchases, {PurchasesOffering} from 'react-native-purchases';
+import { useEffect } from 'react';
+import { useState } from 'react';
 export default function TabOneScreen() {
+  const [onboardingOffering, setOnboardingOffering] = useState<PurchasesOffering>();
+  useEffect( () => {
+    const fetchOfferings = async () => {
+      try {
+        const offerings = await Purchases.getOfferings();
+        console.log(JSON.stringify(offerings.all.onboarding,null,2));
+        if (offerings.current !== null) {
+          // Display current offering with offerings.current
+        }
+      } catch (e) {
+        console.log("Not working")
+      }
+    }
+  })
 
   const  isSubscribed = async () => {
     const paywallResult: PAYWALL_RESULT = await RevenueCatUI.presentPaywallIfNeeded({
@@ -27,7 +44,7 @@ export default function TabOneScreen() {
 
   const proAction = async () => {
     if (await isSubscribed()) {
-      Alert.alert('You are a PRO user!')
+      Alert.alert('User has access!')
     } else {
       Alert.alert('You are not a PRO user!')
     }
@@ -39,6 +56,11 @@ export default function TabOneScreen() {
       <EditScreenInfo path="app/(tabs)/index.tsx" /> */}
 
       <Button title="Perform a PRO action" onPress={proAction}></Button>
+
+      <Button title="Display default paywall" onPress={() => RevenueCatUI.presentPaywall()}></Button>
+
+      <Button title="Display onboarding paywall" onPress={() => RevenueCatUI.presentPaywall({offering: onboardingOffering})}></Button>
+
     </View>
   );
 }
